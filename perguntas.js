44685df1166273fa568817ad0b5a9f028f67d6fb
Lucas <lucas38457445@gmail.com>
2024-11-20 23:00:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Estreia',
+    'Primeira aparição?',
+    'Sonic apareceu primeiro em um game de corrida para fliperama chamado “Rad Mobile”'
+)
+
+criaCartao(
+    'Casa',
+    'De onde ele veio?',
+    'Green Hill, na South Island'
+)
+
+criaCartao(
+    'Idade',
+    'Qaul a sua idade?',
+    '15 anos'
+)
+
+criaCartao(
+    'Vendas',
+    'Qual o jogo mais vendido?',
+    'Sonic 1'
+)
